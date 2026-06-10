5d3354eeb9a3a876826b50c6d7249bae23e67263
nishi kant kumar <nishikants192@gmail.com>
2026-06-10 15:29:22 +0530

    add wrapAsync for async error handling

--- server/controller/auth_controller.js
+++ server/controller/auth_controller.js
@@ -2,8 +2,9 @@ const ExpressError = require("../utils/ExpressError");
 const bcrypt=require("bcryptjs")
 const User=require("../models/userSchema.js");
 const jwt=require("jsonwebtoken");
+const { wrapAsync } = require("../utils/wrapAsync.js");
 
-module.exports.registerUser=async(req,res,next)=>{
+module.exports.registerUser=wrapAsync(async(req,res,next)=>{
     const {username,email,password}=req.body;
     if(!username || !email || !password) return next(new ExpressError(401,"Missing credentials"));
     const isExist=await User.findOne({email});
@@ -22,8 +23,8 @@ module.exports.registerUser=async(req,res,next)=>{
         maxAge:7*24*60*60*1000,
     })
     return res.json({success:true,message:"Register successfully",user});
-}
-module.exports.login=async(req,res,next)=>{
+})
+module.exports.login=wrapAsync(async(req,res,next)=>{
     const {email,password}=req.body;
     if(!email || !password) return next(new ExpressError(401,"Details are missing"));
     const user=await User.findOne({email});
@@ -38,13 +39,13 @@ module.exports.login=async(req,res,next)=>{
         maxAge:7*24*60*60*1000
     })
     return res.json({success:true,message:"Login successful",user});
-}
-module.exports.isLoggedIn=async(req,res,next)=>{
+})
+module.exports.isLoggedIn=wrapAsync(async(req,res,next)=>{
     const {userId}=req.user;
     const userInfo=await User.findById(userId);
     if(!userInfo) return next(new ExpressError(400,"user not found"));
     return res.json({success:true,userInfo});
-}
+})
 module.exports.Logout=(req,res)=>{
     res.clearCookie("userToken",{
         httpOnly:true,
--- server/controller/data_controller.js
+++ server/controller/data_controller.js
@@ -1,13 +1,14 @@
 const Data=require("../models/dataSchema.js");
 const ExpressError = require("../utils/ExpressError.js");
 const mongoose=require("mongoose");
-module.exports.fetchTask=async(req,res,next)=>{
+const { wrapAsync } = require("../utils/wrapAsync.js");
+module.exports.fetchTask=wrapAsync(async(req,res,next)=>{
     const {userId}=req.user;
     const fetchedData=await Data.find({relatedUser:new mongoose.Types.ObjectId(userId)});
     if(!fetchedData) return next(new ExpressError(400,"Task is Empty!"));
     return res.json({success:true,fetchedData});
-}
-module.exports.createTask=async(req,res,next)=>{
+})
+module.exports.createTask=wrapAsync(async(req,res,next)=>{
     const {userId}=req.user;
     const {title,description}=req.body;
     if(!title || !description) return next(new ExpressError(401,"Field is missing"));
@@ -17,14 +18,14 @@ module.exports.createTask=async(req,res,next)=>{
         relatedUser:new mongoose.Types.ObjectId(userId),
     })
     return res.json({success:true,message:"Task added successfully",newTask});
-}
-module.exports.fetchUpdatingTask=async(req,res)=>{
+})
+module.exports.fetchUpdatingTask=wrapAsync(async(req,res)=>{
     const {id}=req.params;
     const task=await Data.findById(id);
     if(!task) return next(new ExpressError(400,"Page not found"));
     return res.json({success:true,task});
-}
-module.exports.updateTask=async(req,res,next)=>{
+})
+module.exports.updateTask=wrapAsync(async(req,res,next)=>{
     const {userId}=req.user;
     const {id}=req.params;
     const task=await Data.findById(id);
@@ -33,8 +34,8 @@ module.exports.updateTask=async(req,res,next)=>{
     const updatedTask=Object.assign(task,req.body);
     await updatedTask.save();
     return res.json({success:true,message:"Updated successfully"});
-}
-module.exports.deleteTask=async(req,res,next)=>{
+})
+module.exports.deleteTask=wrapAsync(async(req,res,next)=>{
     const {userId}=req.user;
     const {id}=req.params;
     const task=await Data.findById(id);
@@ -42,8 +43,8 @@ module.exports.deleteTask=async(req,res,next)=>{
     if(task.relatedUser.toString()!==userId) return next(new ExpressError(403,"permission denied"));
     const deletedTask=await Data.findByIdAndDelete(id);
     return res.json({success:true,message:"Deleted successfully",deletedTask});
-}
-module.exports.handleStatus=async(req,res,next)=>{
+})
+module.exports.handleStatus=wrapAsync(async(req,res,next)=>{
     const {userId}=req.user;
     const {id}=req.params;
     const task=await Data.findById(id);
@@ -53,8 +54,8 @@ module.exports.handleStatus=async(req,res,next)=>{
     else task.taskStatus="Pending";
     await task.save();
     return res.json({success:true,message:"Status changed",task});
-}
-module.exports.filterTask=async(req,res,next)=>{
+})
+module.exports.filterTask=wrapAsync(async(req,res,next)=>{
     const {search}=req.query;
     if(!search?.trim()) return next(new ExpressError(400,"search is required"));
     const matchedTask=await Data.find({
@@ -65,4 +66,4 @@ module.exports.filterTask=async(req,res,next)=>{
     ]})
     if(!matchedTask) return next(new ExpressError(400,"Seach not found"));
     return res.json({success:true,matchedTask});
-}
+})
