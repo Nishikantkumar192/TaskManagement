c3c0c28bffbf520cc37c7f9ac3106ba8fd0d805b
nishi kant kumar <nishikants192@gmail.com>
2026-06-10 13:37:55 +0530

    fixing issue

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const cookieParser=require("cookie-parser");
 const ExpressError = require("./utils/ExpressError.js");
 const cookie=require("cookie");
 const cors=require("cors");
-const allowedOrigin=["task-management-git-main-nishi-kant-kumar.vercel.app","http://localhost:5173"]
+const allowedOrigin=["https://task-management-tawny-tau.vercel.app/","http://localhost:5173"]
 app.use(cors({origin:allowedOrigin,credentials:true}));
 app.use(cookieParser());
 app.use(express.json());
